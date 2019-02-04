5ef445cc4460c6d932b2dcaa60a02085090f4f4f
thamizhselvan93 <thamizhtechie@gmail.com>
2019-02-04 18:57:50 +0530

    Updates

---
+++ precache-manifest.8339278d1123b196d50b009cbfc1db1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41b7f7dd180925c74d07",
+    "url": "/loginModule/static/js/runtime~main.41b7f7dd.js"
+  },
+  {
+    "revision": "abe50ae30742e1213320",
+    "url": "/loginModule/static/js/main.abe50ae3.chunk.js"
+  },
+  {
+    "revision": "b83ac2bdfae092958dea",
+    "url": "/loginModule/static/js/1.b83ac2bd.chunk.js"
+  },
+  {
+    "revision": "abe50ae30742e1213320",
+    "url": "/loginModule/static/css/main.d2f0ee6f.chunk.css"
+  },
+  {
+    "revision": "bee0141eff5eb00c91289b0b3a11b923",
+    "url": "/loginModule/index.html"
+  }
+];
